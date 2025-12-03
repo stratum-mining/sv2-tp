3f6838428bc694b33befecfd05ca56dc58fa21b3
Sjors Provoost <sjors@sprovoost.nl>
2025-12-03 15:24:14 +0100

    Use CoinbaseTemplate if available
    Avoid the brittle process of constructing the template
    coinbase fields from Bitcoin Core's dummy coinbase transaction.
    
    This uses the new getCoinbaseTx() interface method. Fall back to the
    old approach if that method is not available.

--- src/test/sv2_mock_mining.h
+++ src/test/sv2_mock_mining.h
@@ -58,6 +58,7 @@ public:
     CBlock getBlock() override;
     std::vector<CAmount> getTxFees() override;
     std::vector<int64_t> getTxSigops() override;
+    node::CoinbaseTxTemplate getCoinbaseTx() override;
     CTransactionRef getCoinbaseRawTx() override;
     std::vector<unsigned char> getCoinbaseCommitment() override;
     int getWitnessCommitmentIndex() override;
